6fb3013ee0b05e310ad8b7ecb8e0b8173f1bea63
radwa <mail2radwa@gmail.com>
2023-02-27 12:13:44 +0200

    test: Add tests for the product model

---
+++ src/models/tests/product_spec.ts
@@ -0,0 +1,80 @@
+import { ProductStore } from '../product';
+
+const store = new ProductStore()
+
+describe("Product Model", () => {
+  it('should have an index method', (): void => {
+    expect(store.index).toBeDefined();
+  });
+
+  it('should have a show method', (): void => {
+    expect(store.show).toBeDefined();
+  });
+
+  it('should have a create method', (): void => {
+    expect(store.create).toBeDefined();
+  });
+
+  it('should have a update method', (): void => {
+    expect(store.update).toBeDefined();
+  });
+
+  it('should have a delete method', (): void => {
+    expect(store.delete).toBeDefined();
+  });
+  
+  it('create method should add a product', async (): Promise<void> => {
+    const result = await store.create({
+      id: 1,
+      name: 'Skipping Jump Rope',
+      category: 'Sporting Goods',
+      price: 20,
+      description: 'This skipping rope is made of high-quality material and does not break easily.'
+    });
+    expect(result).toEqual({
+      id: 1,
+      name: 'Skipping Jump Rope',
+      category: 'Sporting Goods',
+      price: 20,
+      description: 'This skipping rope is made of high-quality material and does not break easily.'
+    });
+  });
+
+  it('index method should return a list of products', async (): Promise<void> => {
+    const result = await store.index();
+    expect(result).toEqual([{
+      id: 1,
+      name: 'Skipping Jump Rope',
+      category: 'Sporting Goods',
+      price: 20,
+      description: 'This skipping rope is made of high-quality material and does not break easily.'
+    }]);
+  });
+
+  it('show method should return the correct product', async (): Promise<void> => {
+    const result = await store.show("1");
+    expect(result).toEqual({
+      id: 1,
+      name: 'Skipping Jump Rope',
+      category: 'Sporting Goods',
+      price: 20,
+      description: 'This skipping rope is made of high-quality material and does not break easily.'
+    });
+  });
+
+  it('update method should update the product ', async (): Promise<void> => {
+    const edits = {
+      name: 'Skipping Jump Rope with Wooden Handles',
+      price: 30
+    };
+    const result = await store.update("1", edits);
+    expect(result.name).toEqual(edits.name);
+    expect(result.price).toEqual(edits.price);
+  });
+
+  it('delete method should remove the product', async (): Promise<void> => {
+    store.delete("1");
+    const result = await store.index()
+    expect(result).toEqual([]);
+  });
+});
